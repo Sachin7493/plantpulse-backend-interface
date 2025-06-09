582748188d4ae55e663875685f43647427eb4cab
Sachin Kumar Mahato <sachinkumar200216@gmail.com>
2025-06-09 23:29:18 +0530

    add all backend files and folders

---
+++ index.js
@@ -0,0 +1,29 @@
+import express from "express";
+import dotenv from "dotenv";
+import mongoose from "mongoose";
+import cors from "cors";
+import cookieParser from "cookie-parser";
+
+import { chatbotRoute } from "./routes/chatbot.js";
+
+dotenv.config();
+
+import { UserRouter } from "./routes/user.js";
+
+const app = express();
+app.use(express.json());
+
+app.use(cors({ origin: ["http://localhost:5173"], credentials: true }));
+
+app.use(cookieParser());
+app.use("/auth", UserRouter);
+
+app.use("/api", chatbotRoute);
+
+mongoose
+  .connect("mongodb://127.0.0.1:27017/plant")
+  .then(console.log("MongoDB connected"));
+
+app.listen(process.env.PORT, () => {
+  console.log("Server is running");
+});
